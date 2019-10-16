3debae9bb703448cc7c33ca167cb44bff8fcb411
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-16 15:54:07 -0300

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "airbnb"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-indent': 'off',
+    'react/jsx-props-no-spreading': 'off',
+
+    'arrow-parens': ['error', 'as-needed'],
+    'function-paren-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    indent: 'off',
+    'operator-linebreak': 'off',
+    'object-curly-newline': 'off',
+  },
+  settings: {
+    'import/resolver': 'webpack',
+  },
 };
