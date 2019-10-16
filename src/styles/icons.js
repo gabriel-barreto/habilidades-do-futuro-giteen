a7c693a9f556860e307679ddbcbd0d1e064b4891
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-16 20:52:48 -0300

    Import and export WhatsApp icon

--- src/styles/icons.js
+++ src/styles/icons.js
@@ -1,5 +1,7 @@
 import { LinkAlt as link } from 'styled-icons/boxicons-regular/LinkAlt';
+import { Whatsapp as whatsapp } from 'styled-icons/fa-brands/Whatsapp';
 
 export const Link = link;
+export const WhatsApp = whatsapp;
 
-export default { Link };
+export default { Link, WhatsApp };
