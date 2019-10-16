083b1366e3731057230d33114ba3d37429984f3f
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-16 20:52:19 -0300

    Remove size from video component style

--- src/components/Video/styled.js
+++ src/components/Video/styled.js
@@ -2,14 +2,13 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   align-items: center;
-  background-color: var(--darkest);
   display: flex;
+  height: 100%;
   justify-content: center;
 `;
 
 export const Video = styled.iframe`
   margin: auto;
-  min-height: 32rem;
   height: 100%;
   width: 100%;
 `;
