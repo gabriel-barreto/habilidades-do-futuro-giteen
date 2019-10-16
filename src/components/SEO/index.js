396a6b03b2ce1fa5a49d60939d3e52fb7003c665
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-16 16:04:02 -0300

    Add SEO component

---
+++ src/components/SEO/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+
+import DefaultMeta from './content';
+
+function SEO({ author, description, lang, meta, title }) {
+  return (
+    <Helmet
+      htmlAttributes={{ lang }}
+      title={title}
+      titleTemplate="%s | Casa da Pizza"
+      meta={[
+        {
+          name: 'description',
+          content: description,
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          content: description,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          name: 'twitter:creator',
+          content: author,
+        },
+        {
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          name: 'twitter:description',
+          content: description,
+        },
+      ].concat(meta)}
+    />
+  );
+}
+
+SEO.defaultProps = DefaultMeta;
+
+SEO.propTypes = {
+  author: PropTypes.string,
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+};
+
+export default SEO;
