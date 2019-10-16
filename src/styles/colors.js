f8c8aec123a8e51e3f67e74376abbebf0ea6978e
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-16 16:04:13 -0300

    Add and export global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,17 @@
+import { css } from 'styled-components';
+
+const colors = css`
+  :root {
+    --primaryColor: #f1d302;
+    --secondaryColor: #e0271b;
+    --auxColor: #20bf55;
+
+    --dark: #212121;
+    --darkest: #000;
+
+    --light: #ededed;
+    --lightest: #fff;
+  }
+`;
+
+export default colors;
