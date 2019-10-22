47b56b55640fcb9836933e3c3e7a05b1e9322f4c
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-22 06:35:14 -0300

    Update development address

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -7,7 +7,7 @@ const headers = {
 
 const Configs = {
   production: { baseURL: 'http://45.77.211.66:5100/api', headers },
-  development: { baseURL: 'http://localhost:5000/api', headers },
+  development: { baseURL: 'http://localhost:5100/api', headers },
 };
 
 export default Object.freeze(Configs[App.mode]);
