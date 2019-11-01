a6c7059f2f6fa92c508f0abca956ab8a0833d94b
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-11-01 14:47:23 -0300

    Use config to link with another page

--- src/components/TitleBar/index.js
+++ src/components/TitleBar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { App } from '../../config';
+
 import { $store } from '../../services';
 
 import * as S from './styled';
@@ -19,7 +21,9 @@ function TitleBar({ lesson, login }) {
         ) : null}
       </S.TitleBarTitleWrapper>
       {!login ? null : (
-        <S.LoginButton to={$store.get().token ? '/minhas-aulas' : '/entrar'}>
+        <S.LoginButton
+          to={$store.get().token ? `${App.publicURL}/minhas-aulas` : '/entrar'}
+        >
           Entrar
         </S.LoginButton>
       )}
