6f08d719cf2048fb188c7a9453a39c9ba77cf24a
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-11-01 06:36:39 -0300

    Use token and push it into req header

--- src/services/student.service.js
+++ src/services/student.service.js
@@ -1,8 +1,14 @@
 import $http from './http.service';
+import $store from './store.service';
 
 const basePath = '/students';
 
-const getLessons = () => $http.get(`${basePath}/lessons`).then(res => res.data);
+const getLessons = () => {
+  const { token: Authorization } = $store.get();
+  return $http
+    .get(`${basePath}/lessons`, { headers: { Authorization } })
+    .then(res => res.data);
+};
 
 const getInfo = email =>
   $http.get(`${basePath}/${email}?key=email`).then(res => res.data);
